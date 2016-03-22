2af79a8eda9b89e4d6f55b605f602fde58b00de7
Ian Itow <ian.itow@isilon.com>
2016-03-22 16:00:11 -0700

    adding host parameter check

--- var/www/fog/service/isi_vm_reboot.php
+++ var/www/fog/service/isi_vm_reboot.php
@@ -3,6 +3,10 @@ require_once('../commons/base.inc.php');
 try
 {
 	$hostname    = $_REQUEST['hostname'];
+	if (!$hostname)
+	{
+		throw new Exception('error please define hostname example: {url}/fog/service/isi_queue_host.php?hostname={name}');
+	}
 	$command = escapeshellcmd('/usr/bin/python /var/www/fog/service/isi_vm_reboot.py -n ').$hostname;
         $output = shell_exec($command);
         print $output;
