6dcacc7f1f86876cf54c00885045d028e41a31ab
Ian Itow <ian.itow@isilon.com>
2016-03-29 17:38:39 -0700

    Adding host kernelArgs changes

---
+++ var/www/fog/service/isi_set_host_kernel_args.php
@@ -0,0 +1,36 @@
+<?php
+require_once('../commons/base.inc.php');
+try
+{
+        $HostManager = new HostManager();
+        $hostname    = $_REQUEST['hostname'];
+        $kernel_args = $_REQUEST['kernel_args'];
+
+        if (!$hostname)
+        {
+        	throw new Exception('error please define hostname example: {url}/fog/service/isi_queue_host.php?hostname={name}&kernel_args={kernel_args}');
+        }
+
+        if (!$kernel_path)
+        {
+        	throw new Exception('error please define kernel_path example: {url}/fog/service/isi_queue_host.php?hostname={name}&kernel_args={kernel_args}');
+        }
+
+        // Get the Host
+        $Host = $HostManager->getHostByName($hostname);
+
+        if (!$Host)
+        {
+        	throw new Exception('error host not found');
+        }
+
+       $Host->set('kernelArgs', $kernel_args);
+       if ($Host->save()) $Datatosend = "#!ok\n";
+       else throw new Exception('#!er: Error adding kernel path');
+        print 'True';
+
+}
+catch (Exception $e)
+{
+        print $e->getMessage();
+}
